29ab06cb2b12fafcca379485b92f59a25207866f
elmaayouf <boubkerelmaayouf@gmail.com>
2023-03-21 15:22:51 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,6 +3,7 @@
 /**
  * print_alphabet - print all alphabet in lowercase
  */
+
 void print_alphabet(void)
 {
 	char letter;
@@ -11,4 +12,4 @@ void print_alphabet(void)
 		_putchar(letter);
 
 	_putchar('\n');
-} 
+}

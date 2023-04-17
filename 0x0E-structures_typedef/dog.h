83663c0a3f672b3981f558a03dcf99325d54ca12
elmaayouf <boubkerelmaayouf@gmail.com>
2023-04-17 09:02:56 -0700

    task3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,12 +21,7 @@ struct dog
 /**
  * dog_t - typedef for structure dog
  */
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

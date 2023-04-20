2c24004c91a4af47267211c8a64e69b657aa043b
elmaayouf <boubkerelmaayouf@gmail.com>
2023-04-20 11:36:20 -0700

    task2 : a function that prints strings, followed by a new line.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		string = va_arg(ArgPtr, char*);
 		if (string == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", string);
 		if (x != (n - 1) && separator != NULL)

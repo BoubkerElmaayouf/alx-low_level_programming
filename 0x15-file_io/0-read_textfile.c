9c8ffec036b4e4f0eb46f50551cf6530e4e774d0
elmaayouf <boubkerelmaayouf@gmail.com>
2023-05-08 16:01:58 -0700

    task0: function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - this function reads a text file
+ * and prints it to the POSIX standard output.
+ * @filename: the name of the file being read
+ * @letters: The maximum number of bytes to be read from the file
+ * Return: The actual number of bytes read and printed to stdout,
+ *         0 if filename is NULL or the function fails.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *bufffer;
+	ssize_t fd;
+	ssize_t bytes_read;
+	ssize_t bytes_write;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	bytes_read = read(fd, buffer, letters);
+	bytes_write = write(STDOUT_FILENO, buffer, bytes_read);
+
+	free(buffer);
+	close(fd);
+	return (bytes_write);
+}
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,9 +2,14 @@
 #define MAIN_H
 
 /* Libraries : */
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 /* Prototypes : */
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
 #endif

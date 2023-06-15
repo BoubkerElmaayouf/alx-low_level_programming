ae8692ebba978afa08de997f8b180f059c70bc3d
elmaayouf <boubkerelmaayouf@gmail.com>
2023-06-15 14:01:02 -0700

    task7: function that inserts a new node at a given position.☄

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,19 +1,65 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - getting the sum of all n in nodes
- *
- * @head: the head of the list
- * Return: the sum of all n in nodes
+ * len_dlistint - computing the length of a double linked list
+ * @h: the head
+ * Return: the size of the list
  */
-int sum_dlistint(dlistint_t *head)
-{
-	int sum = 0;
 
-	for (dlistint_t *curr = head; curr != NULL; curr = curr->next)
+size_t len_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *curr = h;
+	size_t size = 0;
+
+	while (curr != NULL)
 	{
-		sum += curr->n;
+		curr = curr->next;
+		size++;
 	}
 
-	return (sum);
+	return (size);
+}
+
+/**
+ * insert_dnodeint_at_index - this function insert node at the  index
+ * @h: the head of the double list
+ * @idx: index
+ * @n: the data n for the new node
+ * Return: the pointer the newly created node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *curr, *next;
+
+	if (h == NULL || idx > len_dlistint(*h))
+		return (NULL);
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	if (idx == len_dlistint(*h))
+		return (add_dnodeint_end(h, n));
+
+	curr = *h;
+	while (idx > 1)
+	{
+		curr = curr->next;
+		idx--;
+	}
+
+	next = curr->next;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = curr;
+	new_node->next = next;
+	curr->next = new_node;
+	next->prev = new_node;
+
+	return (new_node);
 }

09b0df932ea1f416351d5df82a1b3fb1e49c4cdc
elmaayouf <boubkerelmaayouf@gmail.com>
2023-06-15 12:42:36 -0700

    task1: func returns the number of elements in a linked dlistint_t list.☄

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include "lists.h"
+
+/**
+ * dlistint_len - functin that prints all the 
+ * elements of a dlistint_t list
+ * @h: the head of the  double list
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *pos = h;
+	int x = 0;
+
+	for (; pos; x++)
+	{
+		pos = pos->next;
+	}
+	return (x);
+}

c5792f7e94360802f839a877f65319ddcfa0a563
BoubkerElmaayouf <boubkerelmaayouf@gmail.com>
2023-09-14 03:45:09 -0700

    Task 0: 0. >>> ht = {} ✔

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,29 @@
 #include "hash_tables.h"
+
 /**
- * key_index - Getting the index of the key
- * @key: thee key
- * @size: size of the table
- * Return: INDEX
- **/
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+* hash_table_create - this function Creates a hash table.
+* @size: The size of the array.
+* Return: If an error occurs - NULL.
+*         else - The pointer to the new hash table.
+*/
+hash_table_t *hash_table_create(unsigned long int size)
 {
-	return (hash_djb2(key) % size);
+	hash_table_t *tabl = malloc(sizeof(hash_table_t));
+	unsigned long int i;
+
+	if (tabl == NULL)
+	{
+		return (NULL);
+	}
+
+	tabl->size = size;
+	tabl->array = malloc(sizeof(hash_node_t *) * size);
+	if (tabl->array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		tabl->array[i] = NULL;
+
+	return (tabl);
 }
